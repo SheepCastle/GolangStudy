9285e90ac20c1ef23e199409c1977efaa90cdf4c
SheepCastle <dladidtjd@gmail.com>
2023-02-14 10:39:33 +0900

    2월13일 까지 공부한 내용

--- pass_fail/pass_fail.go
+++ pass_fail/pass_fail.go
@@ -12,6 +12,6 @@ func main() {
 	fmt.Print("Enter a grade: ")
 	reader := bufio.NewReader(os.Stdin)
 	input, err := reader.ReadString('\n') // 에러 반환 값을 다시 변수에 저장합니다.
-	log.Fatal(err) // 에러를 보고 하고 프로그램을 종료합니다.
+	log.Fatal(err)                        // 에러를 보고 하고 프로그램을 종료합니다.
 	fmt.Println(input)
 }
